a1bc91b52ae3f08959900d18de3afeff1f56dd06
Junn Eric Timoteo <juntyms@yahoo.com>
2022-12-29 11:13:24 +0400

    route

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ use App\Http\Controllers\FingerprintController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
+
 Route::get('/connect',[FingerprintController::class,'connect'])->name('connect');
 Route::Get('/getfp',[FingerprintController::class,'getfp']);
-*/
+
 Route::get('/users',[UserController::class,'user'])->name('users');
 
 
