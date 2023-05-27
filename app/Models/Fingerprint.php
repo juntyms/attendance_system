048ae6a5d2f6fc05b029d76d863b268547689b37
Junn Eric Timoteo <juntyms@yahoo.com>
2023-05-27 18:25:37 +0400

    added devie and fingerprint models

--- app/Models/Fingerprint.php
+++ app/Models/Fingerprint.php
@@ -5,14 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Device extends Model
+class Fingerprint extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
-
-    public function devicetype()
-    {
-        return $this->hasOne(DeviceType::class,'id','device_type_id');
-    }
 }
