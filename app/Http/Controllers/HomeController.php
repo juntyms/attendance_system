fdf749c47171fa4f6cf003a8e6aefddbde9c34ce
Junn Eric Timoteo <juntyms@yahoo.com>
2023-06-06 08:46:38 +0400

    updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller
         $att = Attendance::with('device:name')
                     ->get()
                     ->toArray();
-        dd($att);
+        //dd($att);
 
 
         return view('home')->with('studentAttendancechart',$studentAttendance->build());
