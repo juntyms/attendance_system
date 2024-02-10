382c356bccb6cc062d81f44ec1e8d2f9b5373f3b
Junn Eric Timoteo <juntyms@yahoo.com>
2024-02-10 19:03:16 +0800

    updated deployfingerprint with error handling

--- app/Schedules/DeployFingerprints.php
+++ app/Schedules/DeployFingerprints.php
@@ -13,24 +13,40 @@ class DeployFingerprints
         $devices = Device::where('is_master', 0)->get();
 
         foreach($devices as $device) {
-            $tad_factory = new TADFactory(['ip' => $device->ip]);
+            try {
 
-            $tad = $tad_factory->get_instance();
+                $tad_factory = new TADFactory(['ip' => $device->ip]);
 
-            if ($tad->is_alive()) {
-                $fingerprints = Fingerprint::all();
+                $tad = $tad_factory->get_instance();
 
-                foreach($fingerprints as $fingerprint) {
-                    $user_template_data = [
-                        'pin' => $fingerprint->student_id,
-                        'finger_id' => $fingerprint->fingerid, // First fingerprint has 0 as index.
-                        'size' => $fingerprint->size,    // Be careful, this is not string length of $template1_vx9 var.
-                        'valid' => $fingerprint->valid,
-                        'template' => $fingerprint->template
-                    ];
+                if ($tad->is_alive()) {
+                        $fingerprints = Fingerprint::all();
 
-                    $tad->set_user_template($user_template_data);
+                        foreach($fingerprints as $fingerprint) {
+                            $user_template_data = [
+                            'pin' => $fingerprint->student_id,
+                            'finger_id' => $fingerprint->fingerid, // First fingerprint has 0 as index.
+                            'size' => $fingerprint->size,    // Be careful, this is not string length of $template1_vx9 var.
+                            'valid' => $fingerprint->valid,
+                            'template' => $fingerprint->template
+                        ];
+
+                        try {
+
+                            $tad->set_user_template($user_template_data);
+
+                        } catch (\Exception $e) {
+
+                            Log::error('Error setting user template for fingerprint: ' . $e->getMessage());
+
+                            continue;
+                        }
+                    }
                 }
+            } catch (\Exception $outerException) {
+                // Handle the exception from the outer loop (log, report, etc.)
+                Log::error('Error in outer loop: ' . $outerException->getMessage());
+
             }
         }
     }
