3bf143eca5029ca99be42337adb0c9277bb18d56
Junn Eric <juntyms@yahoo.com>
2025-08-13 17:18:49 +0400

    removed room assignments on the sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -14,9 +14,10 @@
             </a>
             <div class="collapse" id="students">
                 <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"><a class="nav-link" href="{{ route('studentleave.index') }}">Student Leave</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('studentleave.index') }}">Student Leave</a>
+                    </li>
                     <li class="nav-item"><a class="nav-link" href="{{ route('student.list') }}"> Student List </a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('student.roomassignment') }}"> Room Assignment </a></li>
+                    {{-- <li class="nav-item"><a class="nav-link" href="{{ route('student.roomassignment') }}"> Room Assignment </a></li> --}}
 
                 </ul>
             </div>
@@ -34,27 +35,29 @@
             </div>
         </li>
         @role('super-admin')
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#settings" aria-expanded="false" aria-controls="settings">
-                <i class="icon-cog menu-icon"></i>
-                <span class="menu-title">Settings</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="settings">
-                <ul class="nav flex-column sub-menu">
-                    @role('super-admin')
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('devices.index') }}"> Devices </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('departments.index') }}"> Departments </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('rooms.index') }}"> Rooms </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('buildings.index') }}"> Buildings </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('report.schedule') }}"> Report Schedule </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('users.index') }}"> Users </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="{{ route('roles.index')}}"> Roles</a></li>
-                    @endrole
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#settings" aria-expanded="false" aria-controls="settings">
+                    <i class="icon-cog menu-icon"></i>
+                    <span class="menu-title">Settings</span>
+                    <i class="menu-arrow"></i>
+                </a>
+                <div class="collapse" id="settings">
+                    <ul class="nav flex-column sub-menu">
+                        @role('super-admin')
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('devices.index') }}"> Devices </a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('departments.index') }}"> Departments </a>
+                            </li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('rooms.index') }}"> Rooms </a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('buildings.index') }}"> Buildings </a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('report.schedule') }}"> Report Schedule
+                                </a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('users.index') }}"> Users </a></li>
+                            <li class="nav-item"> <a class="nav-link" href="{{ route('roles.index') }}"> Roles</a></li>
+                        @endrole
 
-                </ul>
-            </div>
-        </li>
+                    </ul>
+                </div>
+            </li>
         @endrole
     </ul>
 </nav>
